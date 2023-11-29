24f9a7325ce0299ba25adfffbbb2009ebb9c19bd
SAAJAL JAMSHAID <capableboy198@@example.com>
2023-11-29 08:28:36 +0100

    exo 10 commit

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
- var compteur = 0;
- var compteur2 = 0;
+var compteur = 0;
+var compteur2 = 0;
 
- var MaDiv = document.getElementById("Methode2");
- MaDiv.addEventListener("click", UneProcedureQuiChangeLeText);
+var MaDiv = document.getElementById("Methode2");
+MaDiv.addEventListener("click", UneProcedureQuiChangeLeText);
 
- var MaDiv = document.getElementById("Methode3");
- MaDiv.addEventListener("mouseover", UneProcedureQuiSurvole;
+var MaDiv = document.getElementById("Methode3");
+MaDiv.addEventListener("mouseover", UneProcedureQuiSurvole);
 
- var MaDiv = document.getElementById("Methode2");
- MaDiv.addEventListener("keydown", UneProcedureQuiEcrit);
+var MaDiv = document.getElementById("Methode4");
+MaDiv.addEventListener("keydown", UneProcedureQuiEcrit);
 
- funtion UneProcedureQuiEcrit(evenement)  {
-    evenement.target.innerHTML = "On a appuyè sur : "+evenement.key;
- }
+function UneProcedureQuiEcrit(evenement) {
+    evenement.target.innerHTML = "on a appuyé sur : "+evenement.key;
+}
 
- funtion UneProcedureQuiChangeLeText(evenement) {
-    compteur++ 
-    evenement.target.innerHTML = "On m'a cliqué desus "+compteur" fois";
+function UneProcedureQuiChangeLeText(evenement) {
+    evenement.target.className = 'divChanged';
+    compteur++;
+    evenement.target.innerHTML = "on m'a cliqué dessus : "+compteur+ " fois";
+}
 
- }
-
- function UneProcedureQuiSurvole(evenement) {
-    compteur++
-    evenement.target.innerHTML = "On m'a survollé"+compteur2+"  fois";
- }
+function UneProcedureQuiSurvole(evenement) {
+    evenement.target.className = 'survol';
+    compteur2++;
+    evenement.target.innerHTML = "on m'a survolé : "+compteur2+ " fois";
+}
